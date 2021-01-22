28c89173bbb834a24364f4c5a1b15ae565a1ee78
jasperthegeo <70967011+jasperthegeo@users.noreply.github.com>
2021-01-21 23:04:57 -0500

    update

--- static/js/app.js
+++ static/js/app.js
@@ -19,17 +19,40 @@ function allTheGraphs() {
         var idNum = dropdown.property("value");
 
         //use console.log to see the progress
-        
-        console.log(sampleData);
+        //console.log(sampleData);
+
+        //Call the graph functions that I have created
         // metaDataShow(idNum);
         // barChartGraph(idNum);
         // bubblePlot(idNum);
     });
 };
 
-// function optionChanged(idNum) {
+function barChartGraph (fitereSelected) {
 
+    //we have the ingest the data each time for every graph
+    d3.json("static/js/samples.json").then((sampleData) => {
+        var samples = sampleData.samples;
+        var filterData = samples.filter(object => object.id == filterSelected)[0];
+
+        //let's check this with another console.log
+        //console.log(filterData);
+    });
+};
+
+
+
+
+
+//This is the function that changes the graphs based on the value
+//selected in the drop down menu, it calls the other functions to 
+//create their respective graphs. 
+// function optionChanged(idNum) {
+    // barChartGraph(idNum);
+    // bubblePlot(idNum);
+    // metaDataShow(idNum);
 // }
+
 
 
 allTheGraphs();
